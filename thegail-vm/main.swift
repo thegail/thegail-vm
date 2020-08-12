c69766d62b676f6eebfec45ffe3f47bca51d971d
Theodore Gaillard <teddy.gaillard@icloud.com>
2020-08-12 14:21:11 -0700

    Initial Commit

---
+++ thegail-vm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  thegail-vm
+//
+//  Created by Teddy Gaillard on 8/12/20.
+//  Copyright © 2020 thegail. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
